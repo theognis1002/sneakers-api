2284a5afe4fae52b4dac18275ec7e9402d80576f
theognis1002 <theognis1002@gmail.com>
2022-07-01 09:43:05 -0500

    Lint fix

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
         'no-underscore-dangle': 0,
         'no-param-reassign': 0,
         'no-return-assign': 0,
+        'consistent-return': 0,
         camelcase: 0,
     }
 };
--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,18 +1,16 @@
-const express = require("express");
+const express = require('express');
 
-const emojis = require("./emojis");
-const sneakers = require("./sneakers");
+const emojis = require('./emojis');
+const sneakers = require('./sneakers');
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.json({
-		message: "API - 👋🌎🌍🌏",
-	});
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - 👋🌎🌍🌏',
+  });
 });
 
-console.log("test");
-
-router.use("/emojis", emojis);
-router.use("/sneakers", sneakers);
+router.use('/emojis', emojis);
+router.use('/sneakers', sneakers);
 module.exports = router;
